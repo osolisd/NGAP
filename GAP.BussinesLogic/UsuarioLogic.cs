2b3dc315edcf32ed1f64b2a30ba0b21c980bd05b
Oscar Andrés Solís Durango <oscarasolisd@gmail.com>
2016-09-11 15:21:01 -0500

    Adición de modelo y capa logica

---
+++ GAP.BussinesLogic/UsuarioLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAP.BussinesLogic
+{
+    public class UsuarioLogic
+    {
+    }
+}
