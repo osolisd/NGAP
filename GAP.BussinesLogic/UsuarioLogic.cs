4cf877dff5e732d7e1c10fc988c7d505a4e4cc40
Oscar Andrés Solís Durango <oscarasolisd@gmail.com>
2016-09-11 15:31:49 -0500

    Adición de proyecto test

--- GAP.BussinesLogic/UsuarioLogic.cs
+++ GAP.BussinesLogic/UsuarioLogic.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GAP.BussinesLogic
+﻿namespace GAP.BussinesLogic
 {
     public class UsuarioLogic
     {
